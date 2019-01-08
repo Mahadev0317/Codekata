f898ce4e7fdd6dc9ceeb5d5653cc36eb2f427172
MahaDev <mahadev7397@gmail.com>
2019-01-08 10:25:47 +0000

    Update multiples of 5.py

--- multiples of 5.py
+++ multiples of 5.py
@@ -5,18 +5,4 @@ for i in range(0,r+1,n):
   lis.append(i)
 for i in range(1,len(lis)-1):
   print(lis[i],end=" ")
-print(lis[len(lis)-1])n=int(input())
-r=n*5
-lis=[]
-for i in range(0,r+1,n):
-  lis.append(i)
-for i in range(1,len(lis)-1):
-  print(lis[i],end=" ")
-print(lis[len(lis)-1])n=int(input())
-r=n*5
-lis=[]
-for i in range(0,r+1,n):
-  lis.append(i)
-for i in range(1,len(lis)-1):
-  print(lis[i],end=" ")
 print(lis[len(lis)-1])
