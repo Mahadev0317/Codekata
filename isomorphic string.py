0ca2523a9a49853893b5a26f2714a05addffdb1a
MahaDev <mahadev7397@gmail.com>
2019-01-09 22:55:26 +0530

    Create isomorphic string.py

---
+++ isomorphic string.py
@@ -0,0 +1,12 @@
+s1,s2=input().split()
+def check(s):
+  c=0
+  for i in range(len(s1)-1):
+    if s[i]!=s[i+1]:
+      c+=1
+  return c
+print(check(s1),check(s2))
+if check(s1)==check(s2):
+  print("yes")
+else:
+  print("no")
