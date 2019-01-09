1bfddffcf0bb2269171cf30b186b90fb52c5ce97
MahaDev <mahadev7397@gmail.com>
2019-01-09 18:47:07 +0530

    Update index == value.py

--- index == value.py
+++ index == value.py
@@ -4,6 +4,9 @@ li=[]
 for i in range(len(lis)):
   if lis[i]==i:
     li.append(lis[i])
-for i in range(len(li)-1):
-  print(li[i],end=" ")
-print(li[len(li)-1])
+if len(li)!=0:
+  for i in range(len(li)-1):
+    print(li[i],end=" ")
+  print(li[len(li)-1])
+else:
+  print(-1)
