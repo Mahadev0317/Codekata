8cb026f999b80f12df07e070134c85cfdbe552a7
MahaDev <mahadev7397@gmail.com>
2019-01-10 22:58:23 +0530

    Update shortest len of 2 strings.py

--- shortest len of 2 strings.py
+++ shortest len of 2 strings.py
@@ -1,13 +1,10 @@
 n,k=input().split()
-if len(n) and len(k)==1:
+if len(n)<len(k):
   print(n)
+elif len(n)>len(k):
+  print(k)
 else:
-  if len(n)<len(k):
+  if len(n)//2==1:
     print(n)
-  elif len(n)>len(k):
-    print(k)
   else:
-    if len(n)//2==1:
-      print(n)
-    else:
-      print(k)
+    print(k)
