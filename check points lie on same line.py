2ee9a0bc768b61e63ae766504594a68b57a0203b
MahaDev <mahadev7397@gmail.com>
2019-01-31 22:00:29 +0530

    Update check points lie on same line.py

--- check points lie on same line.py
+++ check points lie on same line.py
@@ -6,5 +6,7 @@ for i in range(3):
   lis.append(k)
 if li[0]==li[1]==li[2] or lis[0]==lis[1]==lis[2]:
   print("yes")
+elif li[0]==lis[0] and li[1]==lis[1] and li[2]==lis[2]:
+  print("yes")
 else:
   print("no")
