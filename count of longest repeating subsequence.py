58110d552ef4ca9374940a7fe0a8b5163510c863
MahaDev <mahadev7397@gmail.com>
2019-02-06 15:55:16 +0530

    Create count of longest repeating subsequence.py

---
+++ count of longest repeating subsequence.py
@@ -0,0 +1,18 @@
+n=int(input())
+li=list(map(int,input().split()))
+c=1
+f=0
+lis=[]
+for i in range(len(li)-1):
+  if li[i]==li[i+1]:
+    if f==0:
+      f=1
+      c+=1
+    else:
+      c+=1
+  else:
+    lis.append(c)
+    f=0
+    c=1
+lis.append(c)
+print(max(lis))
