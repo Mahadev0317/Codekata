0e874fd030a0b6853dc7d28921c83c192f8e3fcd
MahaDev <mahadev7397@gmail.com>
2019-02-09 20:47:07 +0530

    Create most repeated letter & count.py

---
+++ most repeated letter & count.py
@@ -0,0 +1,14 @@
+s=input()
+d={}
+for i in s:
+  if i!=" " and i in d:
+    d[i]+=1
+  elif i!=" " and i not in d:
+    d[i]=1
+print(d)
+m=max(d.values())
+op=""
+for key,value in d.items():
+  if value==m:
+    op+=key
+print(m,op)
