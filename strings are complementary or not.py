da94ebbb371040b77bed524cd989edfe762ed6d4
MahaDev <mahadev7397@gmail.com>
2019-02-09 21:48:21 +0530

    Create strings are complementary or not.py

---
+++ strings are complementary or not.py
@@ -0,0 +1,17 @@
+n=input()
+m=input()
+s=n+m
+li=[]
+print(len(s))
+if len(s)==26:
+  for i in s:
+    if i in li:
+      print(i)
+      print("non-complementary")
+      break
+    else:
+      li.append(i)
+  else:
+    print("complementary")
+else:
+  print("non-complementary")
