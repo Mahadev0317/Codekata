2812fb9d48e3b863f2d1f6179dd315fb254a033a
MahaDev <mahadev7397@gmail.com>
2019-02-11 16:29:47 +0530

    Update permutations of string.py

--- permutations of string.py
+++ permutations of string.py
@@ -1,5 +1,10 @@
 from itertools import permutations
 n=list(input())
-p=permutations(n)
-for i in set(p):
-  print("".join(i))
+pe=permutations(n)
+p=[]
+for i in pe:
+  if i not in p:
+    p.append(i)
+for i in range(len(p)-1):
+  print("".join(p[i]),end="\n")
+print("".join(p[len(p)-1]))
