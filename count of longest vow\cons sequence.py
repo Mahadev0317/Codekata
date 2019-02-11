4fee1fbc11e2e13ee0cc90084c34a70159703ec2
MahaDev <mahadev7397@gmail.com>
2019-02-11 12:31:33 +0530

    Create count of longest vow\cons sequence.py

---
+++ count of longest vow\cons sequence.py
@@ -0,0 +1,13 @@
+s=list(input())
+v=["a","e","i","o","u"]
+li=['v' if i in v else 'c' for i in s]
+c=1
+lis=[]
+for i in range(len(li)-1):
+  if li[i]=="v" and li[i+1]=="c" or li[i]=="c" and li[i+1]=="v":
+    c+=1
+  else:
+    lis.append(c)
+    c=1
+lis.append(c)
+print(0 if c==1 else c)
