02de171c5e9b5365e06078592d107399411e67f8
MahaDev <mahadev7397@gmail.com>
2019-02-11 17:23:17 +0530

    Create count of prime of 1 in binary h_20.py

---
+++ count of prime of 1 in binary h_20.py
@@ -0,0 +1,19 @@
+n,k=map(int,input().split())
+def prime(x):
+  for i in range(2,x):
+    if x%i==0:
+      return False
+      break
+  else:
+    return True
+op=0
+for i in range(n,k):
+  c=0
+  s=bin(i)[2:]
+  for j in s:
+    if j=="1":
+      c+=1
+  if c>1:
+    if prime(c):
+      op+=1
+print(op)
