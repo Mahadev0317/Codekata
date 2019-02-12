fc85d35246a4f09ec276f20d378b4841d2741eeb
MahaDev <mahadev7397@gmail.com>
2019-02-12 18:07:08 +0530

    Create count of max "ab" alternating subarray h_33.py

---
+++ count of max "ab" alternating subarray h_33.py
@@ -0,0 +1,18 @@
+li=list(input())
+c=0;m=0;f=0;i=0
+while i<len(li):
+  if li[i]=="a" and f==0:
+    c+=1
+    f=1
+    i+=1
+  elif li[i]=="b" and f==1:
+    c+=1
+    f=0
+    i+=1
+  else:
+    if c>m:
+      m=c
+    i+=1
+    c=0
+    f=0
+print(0 if m==1 else m)
