01db6d3eadea6d107ec2e1a38e38180602a359b1
MahaDev <mahadev7397@gmail.com>
2019-02-12 22:07:01 +0530

    Create sum of 2 ele closer or =0 h_9.py

---
+++ sum of 2 ele closer or =0 h_9.py
@@ -0,0 +1,19 @@
+n=int(input())
+li=list(map(int,input().split()))
+m=max(li)
+f=0;a=0;b=0
+for i in range(0,n-1):
+  for j in range(i+1,n):
+    if (li[i]+li[j])==0:
+      print(li[i],li[j])
+      break
+      f=1
+    else:
+      if (li[i]+li[j])<m:
+        m=li[i]+li[j]
+        a=i
+        b=j
+  if f==1:
+    break
+else:
+  print(li[a],li[b])
