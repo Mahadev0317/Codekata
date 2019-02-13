e231d81644ae11a9d481cb837b455d66b0ffd34c
MahaDev <mahadev7397@gmail.com>
2019-02-13 16:14:25 +0530

    Create star and superstar element h_15.py

---
+++ star and superstar element h_15.py
@@ -0,0 +1,30 @@
+n=int(input())
+li=list(map(int,input().split()))
+star=[];st=[]
+f=0;a=0;l=0
+for i in range(n):
+  if i==n-1:
+    st.append(li[i])
+    l=1
+  for j in range(i+1,n):
+    if li[i]>li[j]:
+      f=1
+    else:
+      break
+  else:
+    if f==1 and i==0:
+      star.append(li[i])
+    elif f==1 and i!=0:
+      st.append(li[i])
+    if f==1 or l==1 and i>0:
+      for k in range(0,i):
+        if li[i]>li[k]:
+          a=1
+        else:
+          break
+      else:
+        star.append(li[i])
+        a=0
+    f=0
+print(*st)
+print(*star)
