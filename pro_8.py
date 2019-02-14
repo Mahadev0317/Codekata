3ada2533e76216e91846ee35d7fd2a912c5e0f82
MahaDev <mahadev7397@gmail.com>
2019-02-14 22:44:35 +0530

    Update pro_8.py

--- pro_8.py
+++ pro_8.py
@@ -1,11 +1,19 @@
 n,q=map(int,input().split())
 li=list(map(int,input().split()))
+def gcd(x,y):
+  while(y):
+    x,y=y,x%y
+  return x
 lis=[]
 for i in range(q):
   l,r=map(int,input().split())
   if l==r:
-    lis.append(li[l-1])
+    lis.append(l)
   else:
-    lis.append(1)
+    g=gcd(li[l-1],li[l])
+    for j in range(l+1,r):
+      print(j)
+      g=gcd(g,li[j])
+    lis.append(g)
 for i in lis:
   print(i)
