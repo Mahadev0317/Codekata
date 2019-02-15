97ff932c01d2b66f1de1800747160df957c61b05
MahaDev <mahadev7397@gmail.com>
2019-02-15 21:46:34 +0530

    Update longest inc subarray pro_25.py

--- longest inc subarray pro_25.py
+++ longest inc subarray pro_25.py
@@ -1,11 +1,13 @@
 n=int(input())
 a=list(map(int,input().split()))
-c=1;li=[]
+c=1;m=1
 for i in range(n-1):
   if a[i]<a[i+1]:
     c+=1
   else:
-    li.append(c)
+    if c>m:
+      m=c
     c=1
-li.append(c)
-print(max(li))     
+if c>m:
+  m=c
+print(m)
