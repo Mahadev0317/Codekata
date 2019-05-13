ac79aab8064884a4b1e007f0d5636b3c074b2fb7
MahaDev <mahadev7397@gmail.com>
2019-05-13 20:49:01 +0530

    Update min and max h_68.cpp

--- min and max h_68.cpp
+++ min and max h_68.cpp
@@ -2,7 +2,7 @@
 #include<string.h>
 
 int main() {
-  int n,arr[20],min,max,x,y;
+  int n,arr[20],min,max,x=0,y=0;
   std::cin>>n;
   for(int i=0;i<n;i++){
     std::cin>>arr[i];
@@ -12,9 +12,9 @@ int main() {
     if(min>arr[i]){
       min=arr[i];
       x=i;}
-    if(max<arr[i])
+    if(max<arr[i]){
       max=arr[i];
-      y=i;
+      y=i;}
   }
   std::cout<<x+1<<" "<<y+1;
 }
