8780ea888fa8b1123a79826bdc167772110b48e7
MahaDev <mahadev7397@gmail.com>
2019-05-13 14:16:55 +0530

    Create distance of u and v h_61.cpp

---
+++ distance of u and v h_61.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int abs(int x, int y){
+  if(x>y)
+    return x-y;
+  else
+    return y-x;
+}
+int main() {
+  int n,arr[20],u,v,a[20],b[20],w=0,y=0,m[20],k=0,min;
+  std::cin>>n;
+  for(int i=0;i<n;i++)
+    std::cin>>arr[i];
+  std::cin>>u>>v;
+  for(int i=0;i<n;i++)
+  {
+    if(arr[i]==u)
+    {  a[w]=i;
+      w++;}
+    else if(arr[i]==v)
+    { b[y]=i;
+      y++; }
+  }
+  for(int i=0;i<w;i++)
+    for(int j=0;j<y;j++)
+      m[k]=abs(a[i],b[j]);
+      k++;
+  for(int i=0;i<k;i++)
+    if (i==0)
+      min=m[0];
+    else if(arr[i]<min)
+      min=arr[i];
+  std::cout<<min;
+}
