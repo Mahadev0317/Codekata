5059a470f8684b75e13f6292fefe5ed5bef964e5
MahaDev <mahadev7397@gmail.com>
2019-05-15 14:07:53 +0530

    Create rotation count h_60.cpp

---
+++ rotation count h_60.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<cstdlib>
+
+int main() {
+  int n,k,arr[20],arr2[20],sum[20];
+  std::cin>>n;
+  for(int i=0;i<n;i++){
+    std::cin>>arr[i];
+  }
+  for(int j=0;j<n;j++){
+    std::cin>>arr2[j];
+  }
+  for(int i=0;i<n;i++){
+    if(arr2[0]==arr[i]){
+      std::cout<<i;
+    }
+  }
+}
