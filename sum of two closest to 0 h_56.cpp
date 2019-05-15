932f31728305b4217d24362108c0fdfc431bf9df
MahaDev <mahadev7397@gmail.com>
2019-05-15 11:53:18 +0530

    Update sum of two closest to 0 h_56.cpp

--- sum of two closest to 0 h_56.cpp
+++ sum of two closest to 0 h_56.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include<cstdlib>
+
 int main() {
-  int n,arr[20],min=0,x=0,y=0;
+  int n,arr[20],min=0,x=0,y=0,a=0;
   std::cin>>n;
   for(int i=0;i<n;i++){
     std::cin>>arr[i];
   }
   for(int i=0;i<n;i++){
     if(i==0){
-      min=abs(abs(arr[i])-abs(arr[i+1]));
+      min=arr[i]+arr[i+1];
     }
     for(int j=i+1;j<n;j++){
-      if(abs(abs(arr[i])-abs(arr[j]))<=min){
-        min=abs(abs(arr[i])-abs(arr[j]));
+      if (abs(arr[i]+arr[j])<=abs(min)){
+        min=arr[i]+arr[j];
         x=i;y=j;
-      }
+      }  
     }
   }
   std::cout<<arr[x]<<" "<<arr[y];
