e1d32ecbe95efcfefc8c65413fce49a88aac6bc4
MahaDev <mahadev7397@gmail.com>
2019-05-17 13:18:25 +0530

    Update reverse words at odd positions h_72.py

--- reverse words at odd positions h_72.py
+++ reverse words at odd positions h_72.py
@@ -2,5 +2,11 @@ s=list(map(str,input().split(" ")))
 for i in range(0,len(s)):
   if (i==0):
     print(s[i][::-1],end=" ")
+  elif(i==len(s)-1):
+    st=s[i][:-1]
+    if (i%2==0):
+      print(st[::-1],end=" ")
+    else:
+      print(st,end=" ")
   else:
     print(s[i],end=" ")
