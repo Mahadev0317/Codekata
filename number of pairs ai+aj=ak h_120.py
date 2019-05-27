5a7e1c6773eff0f616e74ff05a29e673d1321a22
MahaDev <mahadev7397@gmail.com>
2019-05-28 00:39:11 +0530

    Update number of pairs ai+aj=ak h_120.py

--- number of pairs ai+aj=ak h_120.py
+++ number of pairs ai+aj=ak h_120.py
@@ -4,7 +4,6 @@ c=0
 for i in range(len(lis)-2):
    for j in range(i+1,len(lis)-1):
       for k in range(j+1,len(lis)):
-         print(lis[i],lis[j],lis[k])
          if lis[i]+lis[j]==lis[k]:
             c+=1
 print(c)
