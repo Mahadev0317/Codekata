1b6fc4cd5288e84b50c1708f7ad80c20ad5c9e84
MahaDev <mahadev7397@gmail.com>
2019-05-27 17:32:10 +0530

    Update count occurences of n in l,r h_99.py

--- count occurences of n in l,r h_99.py
+++ count occurences of n in l,r h_99.py
@@ -1,16 +1,6 @@
 l,r,n=map(int,input().split())
-op=0
-def occur(x):
-  c=0;i=0
-  while i<len(x):
-    if i+len(str(n))>len(str(x)):
-      break
-    elif x[i:i+len(str(n))] == str(n):
-      c+=1
-      i=i+len(str(n))
-    else:
-      i+=1
-  return c
-for i in range(l,r):
-  op+=occur(str(i))
-print(op)
+c=0
+for i in range(l,r+1):
+   if str(n)in str(i):
+       c=c+1
+print(c)
