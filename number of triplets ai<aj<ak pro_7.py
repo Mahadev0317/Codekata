43e63ff7f4017f8621acbc1e5194e7c1ccab1239
MahaDev <mahadev7397@gmail.com>
2019-05-28 13:22:15 +0530

    Create number of triplets ai<aj<ak pro_7.py

---
+++ number of triplets ai<aj<ak pro_7.py
@@ -0,0 +1,9 @@
+n=int(input())
+lis=list(map(int,input().split()))
+c=0
+for i in range(len(lis)-2):
+   for j in range(i+1,len(lis)-1):
+      for k in range(j+1,len(lis)):
+         if(lis[i]<lis[j]<lis[k]):
+            c+=1
+print(c)
